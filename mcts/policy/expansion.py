a1359e2e79479fd757eb8294d3722e3456f8a9a0
Thean Lim <lim.the@northeastern.edu>
2022-04-05 17:32:19 -0400

    Added comment - expansion.py

--- mcts/policy/expansion.py
+++ mcts/policy/expansion.py
@@ -2,4 +2,8 @@ from mcts.core import State, Action
 from typing import List
 
 def linearExpansion(state:State)->List[Action]:
+  '''
+  Returns a list of actions in a sequence 
+  that are encoded by the state.
+  '''
   return state.getActions()
