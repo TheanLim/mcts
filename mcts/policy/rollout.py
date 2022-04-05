643f4115db259079e57d3f27a406ebcebdb771d8
Thean Lim <lim.the@northeastern.edu>
2022-04-05 17:33:43 -0400

    Added comment - rollout.py

--- mcts/policy/rollout.py
+++ mcts/policy/rollout.py
@@ -3,6 +3,11 @@ from typing import Any
 import random
 
 def randomRollout(state:State)->Any:
+  '''
+  Starting from the provided state, randomly take actions
+  until the terminal state. 
+  Returns the terminal state reward.
+  '''
   while not state.isTerminal():
       action = random.choice(state.getActions())
       state = state.takeAction(action)
