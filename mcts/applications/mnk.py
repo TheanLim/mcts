4feb13ac0aed2790d576ca1eb555dd65cf1ccba2
Thean Lim <lim.the@northeastern.edu>
2022-04-05 18:10:17 -0400

    Add comments to mnk.py

--- mcts/applications/mnk.py
+++ mcts/applications/mnk.py
@@ -6,6 +6,11 @@ from mcts.utils import sumTuple, Random, gamePlay
 from typing import List, Any, Tuple, Dict, Optional, Any
 from copy import deepcopy
 
+'''
+An MNK Action.
+An action is characterize by placing a player's sign (such as "X")
+at (m,n) coordinate of the board.
+'''
 class MNKAction(Action):
   def __init__(self, playerSign:Any, m:int, n:int):
     self.playerSign = playerSign
@@ -18,39 +23,79 @@ class MNKAction(Action):
   def __repr__(self):
     return str(self)
 
-
+'''
+An MNK Game State.
+The Tic-Tac-Toe game is an example of m=n=k=3
+'''
 class MNK(State):
   def __init__(self, m:int, n:int, k:int, playerSigns:List, emptySign:Any="-") -> None:
-      if k > min(m, n):
-        raise Exception("k has to be smaller or equal to min (m, n)")
-      if emptySign in playerSigns:
-        raise Exception("Use different signs for players and default empty cell")
-      self.m = m
-      self.n = n
-      self.k = k
-      self.playerSigns = playerSigns[:]
-      self.playerSignsRotation = playerSigns[:]
-      self.emptySign = emptySign
-      self.lastAction = None
-      self._isTerminal = False
-      self.remainingMoves = m*n
-      self.reward = tuple([0 for i in range(len(playerSigns))]) # draw by default
-      self.board = [[emptySign for j in range(n)] for i in range(m)]
+    '''
+    m: number of rows of the board
+    n: number of cols of the board
+    k: number of connected sign (such as "X") to win. k has to be smaller or equal to min (m, n)"
+    playerSigns: a list of signs, each representing a player. Ex: ["X","O"]
+    emptySign: represents that a particular cell is empty, and available for taking action. 
+                emptySign cannot be the same as any of the playerSigns
+    '''
+    if k > min(m, n):
+      raise Exception("k has to be smaller or equal to min (m, n)")
+    if emptySign in playerSigns:
+      raise Exception("Use different signs for players and default empty cell")
+    self.m = m
+    self.n = n
+    self.k = k
+    self.playerSigns = playerSigns[:]
+    # The first element self.playerSignsRotation represents the currentPlayer
+    # self.playerSignsRotation is a circular queue: the player that took an action
+    # is placed as the end of the queue.
+    self.playerSignsRotation = playerSigns[:]
+    self.emptySign = emptySign
+    self.lastAction = None
+    # Whether it is a terminal state. This is stored to prevent recomputation.
+    self._isTerminal = False
+    self.remainingMoves = m*n
+    # Store rewards in the form of tuple(rewardPlayer1, rewardPlayer2,...)
+    self.reward = tuple([0 for i in range(len(playerSigns))]) # draw by default
+    # Board of m*n filled with emptySign
+    self.board = [[emptySign for j in range(n)] for i in range(m)]
   
   def getBoard(self)->None: 
+    '''
+    Returns a deep-copied board to prevent accidental modification
+    '''
     return deepcopy(self.board)
 
   def getPlayersSigns(self)->List[Any]: 
+    '''
+    Returns a list of player signs
+    '''
     return self.playerSigns
 
-  def getCurrentPlayerSign(self)->Any: 
+  def getCurrentPlayerSign(self)->Any:
+    '''
+    The first element self.playerSignsRotation represents the currentPlayer
+    ''' 
     return self.playerSignsRotation[0]
 
   def getActions(self)->List[MNKAction]:
+    '''
+    Returns available actions to take.
+    Returns all the cells with emptySign
+    '''
     curPlayer = self.getCurrentPlayerSign()
     return [MNKAction(curPlayer, i, j) for i in range(self.m) for j in range(self.n) if self.board[i][j] == self.emptySign]
 
   def takeAction(self, action:MNKAction)->'State':
+    '''
+    Take an action and returns the resulting state.
+    The original state is NOT altered.
+    Only allows to take action on/if:
+    a) Non terminal state
+    b) Empty Cell
+    c) It is the current player's turn
+    After taking an action, the playerSignsRotation is rotated, 
+    remainingMoves, and lastAction are updated accordingly.
+    '''
     if self.isTerminal():
       raise Exception("Cannot take actions in a terminal state.")
     if (
@@ -71,6 +116,10 @@ class MNK(State):
     return stateCopy
   
   def isTerminal(self)->bool:
+    '''
+    Whether it is a terminal state.
+    Returns True if there's a winner or there's no remaining moves
+    '''
     if self._isTerminal:
       return True
     if not self.lastAction: # No action taken yet
@@ -85,7 +134,17 @@ class MNK(State):
       return self.checkWinner() 
   
   def checkWinner(self)->bool:
+    '''
+    Whether there's a winner.
+    There's a winner if a player could connect self.k signs
+    in a row, col, or diagonally.
+    Updates self.reward, and self._isTerminal if needed.
+    '''
     def encodeReward()->None:
+      '''
+      Encodes reward properly
+      (0,0,1,0) means the third player wins and get reward of 1
+      '''
       reward = []
       for sign in self.playerSigns:
         if sign == self.lastAction.playerSign:
@@ -96,7 +155,7 @@ class MNK(State):
     
     lastAction = self.lastAction
     lastPlayerSign = lastAction.playerSign
-    ### Check if there's a winner
+    ######## Check if there's a winner ########
     leftMost = max(0, lastAction.n-(self.k-1))
     rightMost = min(self.n-1, lastAction.n+(self.k-1))
     topMost = max(0, lastAction.m-(self.k-1))
@@ -158,21 +217,34 @@ class MNK(State):
     return False
   
   def getReward(self)->Tuple:
+    '''
+    Get the reward from this game state.
+    The defult reward is a tuple of zeroes
+    '''
+    # Reward comes from winning.
+    self.checkWinner()
     return self.reward
 
-  def __str__(self):
+  def __str__(self)->str:
+    '''
+    Prints the 2D board properly so that each
+    row is separated (newline) from each other
+    '''
     return '\n'.join(map(str, self.board))
 
 def main():
+  # Create a Tic Tac Toe Game State
   TTT = MNK(3,3, 3, ["X", "O"])
+  # Initialize Agents: MCTS agent and a Random agent
   agent1 = MCTS(UCB, linearExpansion, randomRollout, sumTuple)
-  agent2 = Random()
+  agent2 = Random() # Randomly choose an action
   agents = [agent1, agent2]
+  # Specify kwarg for MCTS agent.search()
   agentKwargs=[{'maxIteration':100, 'rewardIdx':[0]}, {}]
 
-  winStatistics = gamePlay( rounds=100, initialState=TTT, agentList = agents, 
+  winStatistics = gamePlay( rounds=50, initialState=TTT, agentList = agents, 
                             agentSigns = ["X", "O"], agentKwargList=agentKwargs, 
-                            printDetails=False)
+                            rewardSumFunc=sumTuple, printDetails=False)
   print(winStatistics)
 
 if __name__ == "__main__":
