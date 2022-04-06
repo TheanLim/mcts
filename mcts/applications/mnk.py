5603d159fe71da82f0c96b18c4e94ca478f69ce0
Thean Lim <lim.the@northeastern.edu>
2022-04-05 20:55:45 -0400

    update mnk example

--- mcts/applications/mnk.py
+++ mcts/applications/mnk.py
@@ -238,14 +238,14 @@ def main():
   TTT = MNK(3,3, 3, ["O", "X"])
   # Initialize Agents: MCTS agent and a Random agent
   agent1 = Random() # Randomly choose an action
-  agent2 = MCTS(UCB, linearExpansion, randomRollout, sumTuple, simPerIter=300)
+  agent2 = MCTS(UCB, linearExpansion, randomRollout, sumTuple, simPerIter=100)
   agents = [agent1, agent2]
   # Specify kwarg for MCTS agent.search()
   agentKwargs=[{}, {'maxIteration':25, 'rewardIdx':[1]}]
 
-  winStatistics = gamePlay( rounds=100, initialState=TTT, agentList = agents, 
+  winStatistics = gamePlay( rounds=1, initialState=TTT, agentList = agents, 
                             agentKwargList=agentKwargs, 
-                            rewardSumFunc=sumTuple, printDetails=False)
+                            rewardSumFunc=sumTuple, printDetails=True)
   print(winStatistics)
 
 if __name__ == "__main__":
